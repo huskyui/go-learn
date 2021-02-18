0391c7d519fe5dbe9f3449f16c8481f091d2c6ac
huskyui <2207019991@qq.com>
2021-02-18 10:55:24 +0800

    add slice

---
+++ ch18_slice/12_multi-dimensional/01_short-slice/main.go
@@ -0,0 +1,71 @@
+package main
+
+func main() {
+	// student := []string{}
+	// students := [][]string{}
+	// fmt.Println(student)
+	// fmt.Println(students)
+	// fmt.Println(students == nil)
+
+	// var student []string
+	// var students [][]string
+	// fmt.Println(student)
+	// fmt.Println(students)
+	// fmt.Println(student == nil)
+	// student := make([]string, 35)
+	// students := make([][]string, 35)
+	// fmt.Println(student)
+	// fmt.Println(students)
+	// fmt.Println(student == nil)
+
+	// student := []string{}
+	// students := [][]string{}
+	// // student[0] = "add"
+	// student = append(student, "add")
+	// fmt.Println(student)
+	// fmt.Println(students)
+
+	// var student []string
+	// var students [][]string
+	// // student[0] = "add"
+	// student = append(student, "add")
+	// fmt.Println(student)
+	// fmt.Println(students)
+
+	// student := make([]string, 35)
+	// students := make([][]string, 35)
+	// student[0] = "Todd"
+	// student = append(student, "Todd")
+	// fmt.Println(student)
+	// fmt.Println(students)
+	// slice := append([]byte("hello"), "world"...)
+	// fmt.Println(slice)
+
+	// var records [][]string
+	// student1 := make([]string, 4)
+	// student1[0] = "0"
+	// student1[1] = "1"
+	// student1[2] = "2"
+	// student1[3] = "3"
+	// records = append(records, student1)
+	// fmt.Println(records)
+	// student2 := make([]string, 4)
+	// student2[0] = "Gomez"
+	// student2[1] = "Lisa"
+	// student2[2] = "92.00"
+	// student2[3] = "96.00"
+	// // store the record
+	// records = append(records, student2)
+	// // print
+	// fmt.Println(records)
+
+	// transactions := make([][]int, 0, 3)
+	// for i := 0; i < 3; i++ {
+	// 	transaction := make([]int, 0, 4)
+	// 	for j := 0; j < 4; j++ {
+	// 		transaction = append(transaction, j)
+	// 	}
+	// 	transactions = append(transactions, transaction)
+	// }
+	// fmt.Println(transactions)
+}

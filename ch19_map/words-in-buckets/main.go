74e9b31ccab3483ee480220ead595ac2d8854548
huskyui <2207019991@qq.com>
2021-02-19 21:43:18 +0800

    read file and make

---
+++ ch19_map/words-in-buckets/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("http://www.gutenberg.org/cache/epub/1661/pg1661.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(res.Body)
+
+	defer res.Body.Close()
+	scanner.Split(bufio.ScanWords)
+
+	// buckets := make([][]string, 12)
+
+	// for scanner.Scan() {
+	// 	word := scanner.Text()
+	// 	n := hashBucket(word, 12)
+	// 	buckets[n] = append(buckets[n], word)
+	// }
+
+	// for i := 0; i < 12; i++ {
+	// 	fmt.Println(i, " - ", len(buckets[i]))
+	// }
+
+	// fmt.Println(len(buckets))
+	// fmt.Println(cap(buckets))
+
+	buckets := make(map[int]map[string]int)
+
+	for i := 0; i < 12; i++ {
+		buckets[i] = make(map[string]int)
+	}
+
+	for scanner.Scan() {
+		word := scanner.Text()
+		n := hashBucket(word, 12)
+		buckets[n][word]++
+	}
+
+	for k, v := range buckets[6] {
+		fmt.Println(v, "\t-", k)
+	}
+
+}
+
+func hashBucket(word string, buckets int) int {
+	var sum int
+	for _, v := range word {
+		sum += int(v)
+	}
+	return sum % buckets
+
+}
